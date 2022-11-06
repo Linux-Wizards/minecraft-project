53d7b869aff3270941149b4c9d3f77e8a20e4fd7
DarkoGNU <42816979+DarkoGNU@users.noreply.github.com>
2022-11-06 15:10:21 +0100

    Plugin: more work on database support

--- SimpleNotepad/src/main/java/io/github/darkognu/simplenotepad/Note.java
+++ SimpleNotepad/src/main/java/io/github/darkognu/simplenotepad/Note.java
@@ -1,13 +1,45 @@
 package io.github.darkognu.simplenotepad;
 
+import jakarta.persistence.Entity;
+
+import java.io.Serializable;
 import java.util.UUID;
 
-public class Note {
-    private final String message;
-    private final UUID playerId;
+public class Note implements Serializable {
+    private Long id;
+    private String title;
+    private String message;
+    private UUID playerId;
 
-    public Note(String message, UUID playerId) {
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public UUID getPlayerId() {
+        return playerId;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setMessage(String message) {
         this.message = message;
+    }
+
+    public void setPlayerId(UUID playerId) {
         this.playerId = playerId;
     }
 }
