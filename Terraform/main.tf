00320a006d16e52c29d2a34c31ff4ce01a2a9c91
DarkoGNU <42816979+DarkoGNU@users.noreply.github.com>
2022-11-11 13:24:54 +0100

    Initial Terraform scripts

---
+++ Terraform/main.tf
@@ -0,0 +1,244 @@
+resource "oci_identity_compartment" "this" {
+  compartment_id = var.tenancy_ocid
+  description    = var.name
+  name           = replace(var.name, " ", "-")
+
+  enable_delete = true
+}
+
+resource "oci_core_vcn" "this" {
+  compartment_id = oci_identity_compartment.this.id
+
+  cidr_blocks  = [var.cidr_block]
+  display_name = var.name
+  dns_label    = "vcn"
+}
+
+resource "oci_core_internet_gateway" "this" {
+  compartment_id = oci_identity_compartment.this.id
+  vcn_id         = oci_core_vcn.this.id
+
+  display_name = oci_core_vcn.this.display_name
+}
+
+resource "oci_core_default_route_table" "this" {
+  manage_default_resource_id = oci_core_vcn.this.default_route_table_id
+
+  display_name = oci_core_vcn.this.display_name
+
+  route_rules {
+    network_entity_id = oci_core_internet_gateway.this.id
+
+    description = "Default route"
+    destination = "0.0.0.0/0"
+  }
+}
+
+resource "oci_core_default_security_list" "this" {
+  manage_default_resource_id = oci_core_vcn.this.default_security_list_id
+
+  dynamic "ingress_security_rules" {
+    for_each = [22, 25565]
+    iterator = port
+    content {
+      protocol = local.protocol_number.tcp
+      source   = "0.0.0.0/0"
+
+      description = "SSH and Minecraft traffic from any origin"
+
+      tcp_options {
+        max = port.value
+        min = port.value
+      }
+    }
+  }
+
+  egress_security_rules {
+    destination = "0.0.0.0/0"
+    protocol    = "all"
+
+    description = "All traffic to any destination"
+  }
+}
+
+resource "oci_core_subnet" "this" {
+  cidr_block     = oci_core_vcn.this.cidr_blocks.0
+  compartment_id = oci_identity_compartment.this.id
+  vcn_id         = oci_core_vcn.this.id
+
+  display_name = oci_core_vcn.this.display_name
+  dns_label    = "subnet"
+}
+
+resource "oci_core_network_security_group" "this" {
+  compartment_id = oci_identity_compartment.this.id
+  vcn_id         = oci_core_vcn.this.id
+
+  display_name = oci_core_vcn.this.display_name
+}
+
+resource "oci_core_network_security_group_security_rule" "this" {
+  direction                 = "INGRESS"
+  network_security_group_id = oci_core_network_security_group.this.id
+  protocol                  = local.protocol_number.icmp
+  source                    = "0.0.0.0/0"
+}
+
+data "oci_identity_availability_domains" "this" {
+  compartment_id = var.tenancy_ocid
+}
+
+data "oci_core_shapes" "this" {
+  for_each = toset(data.oci_identity_availability_domains.this.availability_domains[*].name)
+
+  compartment_id = var.tenancy_ocid
+
+  availability_domain = each.key
+}
+
+data "oci_core_images" "this" {
+  compartment_id = oci_identity_compartment.this.id
+
+  operating_system         = "Oracle Linux"
+  shape                    = local.shapes.flex
+  sort_by                  = "TIMECREATED"
+  sort_order               = "DESC"
+  state                    = "available"
+}
+
+resource "oci_core_instance" "this" {
+  availability_domain = data.oci_identity_availability_domains.this.availability_domains.0.name
+  compartment_id      = oci_identity_compartment.this.id
+  shape               = local.shapes.flex
+
+  display_name         = "Oracle Linux"
+  preserve_boot_volume = false
+
+  metadata = {
+    ssh_authorized_keys = var.ssh_public_key
+  }
+
+  agent_config {
+    are_all_plugins_disabled = true
+    is_management_disabled   = true
+    is_monitoring_disabled   = true
+  }
+
+  availability_config {
+    is_live_migration_preferred = null
+  }
+
+  create_vnic_details {
+    assign_public_ip = false
+    display_name     = "Minecraft - Server"
+    hostname_label   = "minecraft"
+    nsg_ids          = [oci_core_network_security_group.this.id]
+    subnet_id        = oci_core_subnet.this.id
+  }
+
+  shape_config {
+    memory_in_gbs = 18
+    ocpus         = 3
+  }
+
+  source_details {
+    source_id               = data.oci_core_images.this.images.0.id
+    source_type             = "image"
+    boot_volume_size_in_gbs = 50
+    boot_volume_vpus_per_gb = 120
+  }
+
+  lifecycle {
+    ignore_changes = [source_details.0.source_id]
+  }
+}
+
+resource "oci_core_instance" "that" {
+  availability_domain = data.oci_identity_availability_domains.this.availability_domains.0.name
+  compartment_id      = oci_identity_compartment.this.id
+  shape               = local.shapes.flex
+
+  display_name         = "Minecraft - Backup"
+  preserve_boot_volume = false
+
+  metadata = {
+    ssh_authorized_keys = var.ssh_public_key
+  }
+
+  agent_config {
+    are_all_plugins_disabled = true
+    is_management_disabled   = true
+    is_monitoring_disabled   = true
+  }
+
+  availability_config {
+    is_live_migration_preferred = null
+  }
+
+  create_vnic_details {
+    assign_public_ip = false
+    display_name     = "Minecraft - Backup"
+    hostname_label   = "minecraft-backup"
+    nsg_ids          = [oci_core_network_security_group.this.id]
+    subnet_id        = oci_core_subnet.this.id
+  }
+
+  shape_config {
+    memory_in_gbs = 6
+    ocpus         = 1
+  }
+
+  source_details {
+    source_id               = data.oci_core_images.this.images.0.id
+    source_type             = "image"
+    boot_volume_size_in_gbs = 150
+    boot_volume_vpus_per_gb = 120
+  }
+
+  lifecycle {
+    ignore_changes = [source_details.0.source_id]
+  }
+}
+
+
+data "oci_core_private_ips" "that" {
+  ip_address = oci_core_instance.that.private_ip
+  subnet_id  = oci_core_subnet.this.id
+}
+
+resource "oci_core_public_ip" "that" {
+  compartment_id = oci_identity_compartment.this.id
+  lifetime       = "RESERVED"
+
+  display_name  = oci_core_instance.that.display_name
+  private_ip_id = data.oci_core_private_ips.that.private_ips.0.id
+}
+
+resource "oci_core_volume_backup_policy" "this" {
+  count = 2
+
+  compartment_id = oci_identity_compartment.this.id
+
+  display_name = format("Daily %d", count.index)
+
+  schedules {
+    backup_type       = "INCREMENTAL"
+    hour_of_day       = count.index
+    offset_type       = "STRUCTURED"
+    period            = "ONE_DAY"
+    retention_seconds = 172800
+    time_zone         = "REGIONAL_DATA_CENTER_TIME"
+  }
+}
+
+resource "oci_core_volume_backup_policy_assignment" "this" {
+  count = 2
+
+  asset_id = (
+    count.index == 0 ?
+    oci_core_instance.this.boot_volume_id :
+    oci_core_instance.that.boot_volume_id
+  )
+
+  policy_id = oci_core_volume_backup_policy.this[count.index].id
+}
