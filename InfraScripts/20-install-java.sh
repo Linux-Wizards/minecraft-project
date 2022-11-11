d3eca5a32838498dd5fd512ebf6d45cdc3cf43cb
DarkoGNU <42816979+DarkoGNU@users.noreply.github.com>
2022-11-12 00:31:51 +0100

    Add Java installation script

---
+++ InfraScripts/20-install-java.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+java_package="jdk-19-headless"
+
+# Get functions: info, error, exit_on_fail
+source common.sh
+
+info "Beginning Java installation"
+
+yum -y install "$java_package"
+exit_on_fail "Java installation possibly failed"
+
+info "Finished installing Java"
+
