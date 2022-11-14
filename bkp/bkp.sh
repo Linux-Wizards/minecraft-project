df1d8d5925131b1162d23eab3ef9ac5029faf262
imthernet <39921871+imthernet@users.noreply.github.com>
2022-11-14 10:23:40 +0100

    Upgraded initial backup script

--- bkp/bkp.sh
+++ bkp/bkp.sh
@@ -1,35 +1,48 @@
-#!/bin/bash
+#!/bin/bas
+
+info () { echo -e "\e[32m[INFO]\e[0m ${1}" ; }
+warn () { echo -e "\e[33m[WARN]\e[0m ${1}" ; }
+error () { echo -e "\e[31m[ERROR]\e[0m ${1}" ; }
 
 helper()
 {
 cat <<EOF
 Usage: $0 [option]
 
--f|--full      Full server backup. Going by default if user pass is empty
--p|--partial   Without dirs eg. dev,proc,sys,tmp,run,mnt,media
--d|--dry       Trial run without making changes 
+-d|--daily	Daily backup
+-w|--weekly	Weekly backup
+-m|--monthly	Monthly backup
+
 EOF
 }
 
-DIR=$(pwd)
-#for tests --> full path is safer
-SRC=~/bkp_proj/a/b/c
-DST=~/bkp_proj/dest/a/b/c
+DDIR=/home/user/backup/daily
+WDIR=/home/user/backup/weekly
+MDIR=/home/user/backup/monthly
 
 comm=$1
-[[ $# -gt 1 ]] && { echo "** Used too many args. Use only one. **"; exit 0; }
+[[ $# -gt 1 ]] && { warn "** Used too many args. Use only one. **"; exit 0; }
 #[[ $comm == "" ]] && { comm="--full"; } 
 case $comm in
-        "-f"|"--full")
-        	#tutaj info do uzytkownika, za ~1min wylaczenie serwera / serwer STOP / *rsync* / serwer START
-        	echo "rsync -avrAXH $SRC $DST" ;; 
-        "-p"|"--partial")
-        	echo "rsync -avrAXH $SRC/ --exclude {"$SRC/dev/*","$SRC/proc/*","$SRC/sys/*","$SRC/tmp/*","$SRC/run/*","$SRC/mnt/*","$SRC/media/*"}" ;;
-        	## Jakies cos $? jezeli =1 to call an ambulance
-        "-d"|"--dry")
-        	echo "rsync -avrAXH --dry-run $SRC $DST" ;;
-        "-h"|"--help")
-		helper ;;
+        "-d"|"--daily")
+        		#tutaj info do uzytkownika, za ~1min wylaczenie serwera / serwer STOP / *rsync* / serwer START
+       		 	tar -zcf $DDIR/backup-$(date +%Y%m%d).tar.gz -C path/to/backup-dir/ to
+			find $DDIR/* -mtime +7 -delete
+			rsync -a --delete $DDIR/ ubuntu@mcraft.imth.tk:~/backups/daily/ 2>/ramdisk/daily.log
+			;;
+        "-w"|"--weekly")
+        		#tutaj info do uzytkownika, za ~1min wylaczenie serwera / serwer STOP / *rsync* / serwer START
+			tar -zcf $WDIR/backup-$(date +%Y%m%d).tar.gz -C path/to/backup-dir/ to
+                        find $WDIR/* -mtime +31 -delete
+                        rsync -a --delete $WDIR/ ubuntu@mcraft.imth.tk:~/backups/weekly/ 2>/ramdisk/weekly.log
+			;;
+        "-m"|"--monthly")
+        		#tutaj info do uzytkownika, za ~1min wylaczenie serwera / serwer STOP / *rsync* / serwer START
+                        tar -zcf $MDIR/backup-$(date +%Y%m%d).tar.gz -C path/to/backup-dir/ to
+                        find $MDIR/* -mtime +365 -delete
+                        rsync -a --delete $MDIR/ ubuntu@mcraft.imth.tk:~/backups/monthly/ 2>/ramdisk/monthly.log
+			;;
+        "-h"|"--help") helper ;;
 
         *) echo "** Bad input, use -h|--help **" ;;
 esac
