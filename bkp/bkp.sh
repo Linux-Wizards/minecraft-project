e46d8ae381eb4b79f5b2791a1859785c4a583e9a
imthernet <39921871+imthernet@users.noreply.github.com>
2022-11-14 10:29:11 +0100

    Upgraded initial backup script

--- bkp/bkp.sh
+++ bkp/bkp.sh
@@ -22,24 +22,23 @@ MDIR=/home/user/backup/monthly
 
 comm=$1
 [[ $# -gt 1 ]] && { warn "** Used too many args. Use only one. **"; exit 0; }
-#[[ $comm == "" ]] && { comm="--full"; } 
 case $comm in
         "-d"|"--daily")
         		#tutaj info do uzytkownika, za ~1min wylaczenie serwera / serwer STOP / *rsync* / serwer START
        		 	tar -zcf $DDIR/backup-$(date +%Y%m%d).tar.gz -C path/to/backup-dir/ to
-			find $DDIR/* -mtime +7 -delete
+			find $DDIR/* -mtime +7 -delete		#delete older than week
 			rsync -a --delete $DDIR/ ubuntu@mcraft.imth.tk:~/backups/daily/ 2>/ramdisk/daily.log
 			;;
         "-w"|"--weekly")
         		#tutaj info do uzytkownika, za ~1min wylaczenie serwera / serwer STOP / *rsync* / serwer START
 			tar -zcf $WDIR/backup-$(date +%Y%m%d).tar.gz -C path/to/backup-dir/ to
-                        find $WDIR/* -mtime +31 -delete
+                        find $WDIR/* -mtime +31 -delete		#delete older than month
                         rsync -a --delete $WDIR/ ubuntu@mcraft.imth.tk:~/backups/weekly/ 2>/ramdisk/weekly.log
 			;;
         "-m"|"--monthly")
         		#tutaj info do uzytkownika, za ~1min wylaczenie serwera / serwer STOP / *rsync* / serwer START
                         tar -zcf $MDIR/backup-$(date +%Y%m%d).tar.gz -C path/to/backup-dir/ to
-                        find $MDIR/* -mtime +365 -delete
+                        find $MDIR/* -mtime +365 -delete	#delete older than year
                         rsync -a --delete $MDIR/ ubuntu@mcraft.imth.tk:~/backups/monthly/ 2>/ramdisk/monthly.log
 			;;
         "-h"|"--help") helper ;;
