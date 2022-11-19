dca335e452873a1c8c372b1a66a63c7e4ba8d9b5
fafa194 <51638888+fafa194@users.noreply.github.com>
2022-11-19 22:51:29 +0100

    Install-Java changed yum -q removed

--- Installation/Install-Java.sh
+++ Installation/Install-Java.sh
@@ -9,17 +9,18 @@ java_repo_dir="/etc/yum.repos.d/corretto.repo"
 java_repo_url="https://yum.corretto.aws/corretto.repo"
 
 rpm -qa | grep "$java_version"
+exit_code=$?
 
-if [ "$?" -ne 0 ]; then
-	warn "No java 17 available, download required" 
+if [ "$exit_code" -ne 0 ]; then
+        warn "No java 17 available, download required"
 
-	# Java installation 
-	rpm --import "$java_repo_key_url"
-	info "Imported Java repo key" 
-	curl -L -o "$java_repo_dir" "$java_repo_url"
-	info "Added Java repository to yum" 
-	yum install -y -q "$java_version"
-	info "Completed installation succesfully" 
-else 
-	info "$java_version already installed"
+        # Java installation 
+        rpm --import "$java_repo_key_url"
+        info "Imported Java repo key"
+        curl -L -o "$java_repo_dir" "$java_repo_url"
+        info "Added Java repository to yum"
+        yum install -y "$java_version-amazon-corretto*"
+        info "Completed installation succesfully"
+else
+        info "$java_version already installed"
 fi
