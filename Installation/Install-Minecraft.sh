1f302ca3080d90bfcf1a1f240af5f90d4d992132
fafa194 <51638888+fafa194@users.noreply.github.com>
2022-11-19 23:28:38 +0100

    Moved variables to more specific script

--- Installation/Install-Minecraft.sh
+++ Installation/Install-Minecraft.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+# Version configuration
+mc_version="1.19.2"
+paper_build="271"
+
+# Server configuration
+install_dir="server"
+online_mode="false"
+rcon_password="linuxwizards"
+
+# Server jar address 
+download_location="https://api.papermc.io/v2/projects/paper/versions/${mc_version}/builds/${paper_build}/downloads/paper-${mc_version}-${paper_build}.jar"
+
 source /tmp/minecraft-project/common.sh
-echo "$username"
 mkdir -p "$install_dir"  # Creating installation directory for server 
 exit_on_fail "Failed to create directory"  
 info "Created server directory ${PWD}/${install_dir}"
 
 cd "$install_dir" 
-
 
 info "Starting download" 
 #curl -o paper.jar "$download_location"
@@ -23,7 +33,3 @@ echo "online-mode=$online_mode" > server.properties
 echo "spawn-protection=0" >> server.properties
 echo "rcon.password=$rcon_password" >> server.properties
 echo "enable-rcon=true" >> server.properties
-
-
-
-
--- Installation/Start-Installation.sh
+++ Installation/Start-Installation.sh
@@ -1,17 +1,7 @@
 #!/bin/bash 
 
-# Version configuration
-mc_version="1.19.2" 
-paper_build="271"
-
 # Server configuration
 username="minecraft"
-install_dir="server"
-online_mode="false"
-rcon_password="linuxwizards"
-
-# Server jar address 
-download_location="https://api.papermc.io/v2/projects/paper/versions/${mc_version}/builds/${paper_build}/downloads/paper-${mc_version}-${paper_build}.jar"
 
 # Getting useful functions
 source common.sh
@@ -26,7 +16,7 @@ cp -a Install-Minecraft.sh /tmp/minecraft-project/
 info "Beginnig server installation "
 
 ./Install-Java.sh
-sudo --login --user="$username" install_dir="$install_dir" online_mode="$online_mode" rcon_password="$rcon_password" download_location="$download_location" /tmp/minecraft-project/Install-Minecraft.sh
-exit_on_fail "Failed to start installation"
+sudo --login --user="$username" /tmp/minecraft-project/Install-Minecraft.sh
+exit_on_fail "Failed to start minecraft installation"
 
 info "Installation success"
