ade5c20aaea77e49b061e9765b8b5a4f17070e76
fafa194 <51638888+fafa194@users.noreply.github.com>
2022-11-23 21:29:40 +0100

    Adding systemd.servce & Install-systemd.sh

---
+++ Installation/Install-Minecraftd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source common.sh
+service_name=minecraftd.service
+
+# Copying minecraft.service to systemd
+info "Installing minecraft service"
+cp --preserve=mode "$service_name" /etc/systemd/system/
+
+# Enabling service 
+systemctl daemon-reload 
+systemctl enable $service_name
+info "Enabled $service_name"
