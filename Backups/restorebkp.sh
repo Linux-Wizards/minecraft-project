5cc27c46a6cfc932e45fadc0c818f4f4dfa7498c
Wojciech <imternet@pop-os.localdomain>
2022-12-03 15:28:43 +0100

    initial interactive v1.0

--- Backups/restorebkp.sh
+++ Backups/restorebkp.sh
@@ -8,11 +8,32 @@ case $chs in
 	"dump")
 		mkdir -p $(pwd)/restore_dump && cd "$_"
 		rsync $BKPSdump .
+		PS3='Choose number of file which do you want to restore: '
+		select file in $(ls -A1 *.dump*); do
+                        read -p "You are going to restore database with file: $file. Are you sure? (type yes|no): " sel
+                        case $sel in
+                                "yes") echo "mysql -u root -p mc_db < $file"  ;;
+                                "no") echo "bye"; break ;;
+                                *)
+                        esac
+
+                done
+
 		;;
-	"backup") 
+	"backup")
 		mkdir -p $(pwd)/restore_tar $$ cd "$_"
-		cd restore_tar
 		rsync -r $BKPStar .
+	       	PS3='Choose number of file which do you want to restore: '
+		select file in $(ls -A1 *.tar*); do 
+			read -p "You are going to restore $file to $DSTDIR folder. Are you sure? (type yes|no): " sel
+		       	case $sel in 
+				"yes") echo "tar -xvf $file $DSTDIR"  ;;
+				"no") echo "bye"; break ;;
+				*)
+			esac	
+		
+		done
+	
 		;;
 esac
 
