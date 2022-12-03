ff32f99240b13669622cd9ece449432387753ae7
Wojciech <imternet@pop-os.localdomain>
2022-12-03 18:50:48 +0100

    initial interactive v1.1

--- Backups/restorebkp.sh
+++ Backups/restorebkp.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+info () { echo -e "\e[32m[INFO]\e[0m ${1}" ; }
+warn () { echo -e "\e[33m[WARN]\e[0m ${1}" ; }
+error () { echo -e "\e[31m[ERROR]\e[0m ${1}" ; }
+
+fbfile=fullbackup
 DSTDIR=/home/minecraft/
 BKPStar=wojtek@minecraft-backup.inspir.ovh:/home/wojtek/backups/*/*.tar.gz
 BKPSdump=wojtek@minecraft-backup.inspir.ovh:/home/wojtek/backups/*/*.dump
@@ -8,8 +13,8 @@ case $chs in
 	"dump")
 		mkdir -p $(pwd)/restore_dump && cd "$_"
 		rsync $BKPSdump .
-		PS3='Choose number of file which do you want to restore: '
-		select file in $(ls -A1 *.dump*); do
+		PS3='Choose number of a file which do you want to restore: '
+		select file in $(ls -A1 *.dump); do
                         read -p "You are going to restore database with file: $file. Are you sure? (type yes|no): " sel
                         case $sel in
                                 "yes") echo "mysql -u root -p mc_db < $file"  ;;
@@ -23,13 +28,18 @@ case $chs in
 	"backup")
 		mkdir -p $(pwd)/restore_tar $$ cd "$_"
 		rsync -r $BKPStar .
-	       	PS3='Choose number of file which do you want to restore: '
-		select file in $(ls -A1 *.tar*); do 
-			read -p "You are going to restore $file to $DSTDIR folder. Are you sure? (type yes|no): " sel
+	       	PS3='Choose number of a file which do you want to restore, or use ^C to break: '
+		select file in $(ls -A1 *.tar | grep -v "fullbackup.tar"); do 
+			warn; read -p "You are going to restore $file to $DSTDIR folder. Are you sure? (type yes|no): " sel
 		       	case $sel in 
-				"yes") echo "tar -xvf $file $DSTDIR"  ;;
+				"yes")
+					info "Restoring Fullbackup file to $DSTDIR"
+					tar -xzf $fbfile.tar -C $DSTDIR -g /dev/null
+					info "Now restoring from $file to $DSTDIR"
+					exit 0
+					;;
 				"no") echo "bye"; break ;;
-				*)
+				*) error "Bad input"; break
 			esac	
 		
 		done
